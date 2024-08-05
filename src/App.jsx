4a38aad2f5c9c8e6e1eee485ad50d589e5b3fb2d
John Lloyd Umani <umani.johnlloyd.esplana@gmail.com>
2024-08-05 18:14:41 +0800

    refactor: bugfix for navigations id

--- src/App.jsx
+++ src/App.jsx
@@ -46,19 +46,19 @@ export default function App() {
         className='flex flex-col mt-[4rem] lg:flex-row lg:gap-7 lg:bg-background-alt lg:p-10'
       >
         <div className='lg:flex lg:flex-col lg:gap-7 lg:w-[60%]'>
-          <motion.div {...getAnimationProps(-20, 0, 0.1)}>
+          <motion.div {...getAnimationProps(-20, 0, 0.1)} id='#aboutme1'>
             <Hero className={`lg:pt-10 cardSection lg:from-background-component1 lg:bg-background-alt lg:hover:bg-background transition-colors`} />
           </motion.div>
           <div className='lg:flex lg:flex-row lg:gap-7'>
-            <motion.div className='lg:w-1/2' {...getAnimationProps(0, 20, 0.2)}>
+            <motion.div className='lg:w-1/2' {...getAnimationProps(0, 20, 0.2)} id='#aboutme2'>
               <AboutMe className={`cardSection lg:from-background-component2 lg:hover:bg-background transition-colors`} />
             </motion.div>
-            <motion.div className='lg:w-1/2' {...getAnimationProps(0, 20, 0.3)}>
+            <motion.div className='lg:w-1/2' {...getAnimationProps(0, 20, 0.3)} id='#education'>
               <Education className={`cardSection lg:from-background-component3 lg:bg-background-alt lg:hover:bg-background transition-colors`} />
             </motion.div>
           </div>
         </div>
-        <motion.div className='lg:w-[40%]' {...getAnimationProps(20, 0, 0.4)}>
+        <motion.div className='lg:w-[40%]' {...getAnimationProps(20, 0, 0.4)} id='#skills'>
           <Skills className={`cardSection lg:from-background-component4 lg:hover:bg-background transition-colors`} />
         </motion.div>
       </motion.div>
