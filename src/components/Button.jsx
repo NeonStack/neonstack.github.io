08bbd48afe3c414811bcdfedb8a874f86d41822b
NeonStack <umani.johnlloyd.esplana@gmail.com>
2026-02-06 14:31:13 +0800

    Updates

--- src/components/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-const Button = ({href, addClass, children }) => {
-    const baseClass = "py-3 px-5 text-primary font-extrabold text-[1.1rem] rounded-md";
-    return <a href={href} className={`${baseClass} ${addClass}`}>{children}</a>;
-}
-
-export default Button;
